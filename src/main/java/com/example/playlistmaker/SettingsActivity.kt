ed58f83faf596da4f851c115bdb2841659cf6e2f
BOGDAN1986 <hollowman1986@yandex.ru>
2023-11-08 23:00:20 +0300

    my Commit 3 Домашняя работа 8 спринт

---
+++ src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,22 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setings)
+
+        val backButton = findViewById<Button>(R.id.BackButton)
+
+        backButton.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+        }
+    }
+}
